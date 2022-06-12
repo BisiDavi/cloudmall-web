ab1f1be6d7f91b1c005bd48d31b7662751ac9751
BisiDavi <oludavidconnect@gmail.com>
2022-06-12 03:26:20 +0100

    search feature updated

--- components/search/SearchStore.tsx
+++ components/search/SearchStore.tsx
@@ -1,21 +1,36 @@
 import SearchIcon from "@/components/icons/SearchIcon";
-import { useAppDispatch } from "@/hooks/useRedux";
+import { useAppDispatch, useAppSelector } from "@/hooks/useRedux";
 import { updateSearch } from "@/redux/search-slice";
+import Image from "next/image";
 
 export default function SearchStore() {
   const dispatch = useAppDispatch();
+  const { search } = useAppSelector((state) => state.search);
 
   function inputHandler(e: any) {
     dispatch(updateSearch(e.target.value));
   }
 
+  function resetHandler() {
+    dispatch(updateSearch(""));
+  }
+
   return (
     <>
       <div className="search-store">
-        <span>
+        <span className="search">
           <SearchIcon />
         </span>
-        <input className="search-input" onChange={inputHandler} />
+        <input
+          value={search}
+          className="search-input"
+          onChange={inputHandler}
+        />
+        {search && (
+          <button className="cancel" type="button" onClick={resetHandler}>
+            <Image src="/cancelIcon.png" height={15} width={15} alt="cancel" />
+          </button>
+        )}
       </div>
       <style jsx>
         {`
@@ -23,10 +38,17 @@ export default function SearchStore() {
             display: flex;
             position: relative;
           }
-          .search-store span {
+          .search-store span.search {
             position: absolute;
             top: 20px;
             left: 15px;
+          }
+          .search-store button.cancel {
+            position: absolute;
+            top: 18px;
+            right: 15px;
+            border: none;
+            background-color: transparent;
           }
           .search-input {
             width: 100%;
