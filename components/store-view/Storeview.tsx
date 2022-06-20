c6057ef1bb3d8a71fb53fe026d1c7b68035206d4
BisiDavi <oludavidconnect@gmail.com>
2022-06-20 05:09:25 +0100

    store view updated

---
+++ components/store-view/Storeview.tsx
@@ -0,0 +1,30 @@
+import { useQuery } from "react-query";
+
+import RestaurantPillsGroup from "@/components/pills/RestaurantPillsGroup";
+import ProductGridView from "@/components/product/ProductGridView";
+import StoreLayoutPage from "@/layout/store-layout";
+import { storeProfile } from "@/utils/storeRequest";
+
+interface Props {
+  storeId: string;
+}
+
+export default function Storepageview({ storeId }: Props) {
+  const { data, status } = useQuery(`store-profile-${storeId}`, () =>
+    storeProfile(storeId)
+  );
+
+  const storeName = status === "success" ? data?.data?.store?.name : "";
+
+  return (
+    <StoreLayoutPage title={storeName} padding="0px 0px 0px 0px">
+      {storeId !== undefined && (
+        <RestaurantPillsGroup
+          storeType="store"
+          category={data?.data?.store?.category?.name}
+        />
+      )}
+      {storeId !== undefined && <ProductGridView storeId={storeId} />}
+    </StoreLayoutPage>
+  );
+}
--- pages/store/[...store].tsx
+++ pages/store/[...store].tsx
@@ -1,29 +1,10 @@
 import { useRouter } from "next/router";
-import { useQuery } from "react-query";
 
-import RestaurantPillsGroup from "@/components/pills/RestaurantPillsGroup";
-import ProductGridView from "@/components/product/ProductGridView";
-import StoreLayoutPage from "@/layout/store-layout";
-import { storeProfile } from "@/utils/storeRequest";
+import Storepageview from "@/components/store-view/Storeview";
 
 export default function StorePage() {
   const router: any = useRouter();
   const storeId = router?.query?.store_id;
-  const { data, status } = useQuery(`store-profile-${storeId}`, () =>
-    storeProfile(storeId)
-  );
 
-  const storeName = status === "success" ? data?.data?.store?.name : "";
-
-  return (
-    <StoreLayoutPage title={storeName} padding="0px 0px 0px 0px">
-      {storeId !== undefined && (
-        <RestaurantPillsGroup
-          storeType="store"
-          category={data?.data?.store?.category?.name}
-        />
-      )}
-      {storeId !== undefined && <ProductGridView storeId={storeId} />}
-    </StoreLayoutPage>
-  );
+  return <>{storeId !== undefined && <Storepageview storeId={storeId} />}</>;
 }
