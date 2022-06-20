d48a471410323b096015e9f258bb6fec25d5d532
BisiDavi <oludavidconnect@gmail.com>
2022-06-20 03:42:35 +0100

    cart loader updated

--- components/cart/CartGroup.tsx
+++ components/cart/CartGroup.tsx
@@ -1,6 +1,7 @@
 import CartItem from "@/components/cart/CartItem";
 import useCart from "@/hooks/useCart";
 import CartFooter from "@/components/cart/CartFooter";
+import Image from "next/image";
 
 export default function CartGroup() {
   const { useGetCart } = useCart();
@@ -8,13 +9,20 @@ export default function CartGroup() {
 
   return (
     <>
-      {status === "error"
-        ? "error occured"
-        : status === "loading"
-        ? "loading..."
-        : cart?.items.map((cartItem: any) => (
-            <CartItem item={cartItem} key={cartItem._id} />
-          ))}
+      {status === "error" ? (
+        "error occured"
+      ) : status === "loading" ? (
+        <Image
+          src="/shopping-cart.gif"
+          alt="cart loading"
+          height={300}
+          width={300}
+        />
+      ) : (
+        cart?.items.map((cartItem: any) => (
+          <CartItem item={cartItem} key={cartItem._id} />
+        ))
+      )}
       <CartFooter total={cart?.fees?.total} />
       <div className="bottom" />
       <style jsx>
