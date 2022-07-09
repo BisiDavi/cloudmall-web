57925643e5a505c9613ac987d6f3723c766a7644
BisiDavi <oludavidconnect@gmail.com>
2022-07-09 07:30:44 +0100

    url updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import Storeview from "@/components/store-view";
 import useBaseUrl from "@/hooks/useBaseUrl";
@@ -9,29 +9,37 @@ import { whatsappSignin } from "@/utils/authRequest";
 
 export default function Home() {
   const { baseURL } = useBaseUrl();
+  const [waCode, setWaCode] = useState("");
   const toastID = useRef(null);
   const { loadingToast, updateToast } = useToast();
 
   useEffect(() => {
-    const waCode = window.location.href.split("?waCode=")[1];
-
-    console.log("waCode", waCode);
-
-    if (baseURL.length === 0 && typeof window !== "undefined") {
-      loadingToast(toastID);
-      whatsappSignin(baseURL, {
-        waCode,
-        rememberMe: true,
-      })
-        .then((response) => {
-          updateToast(toastID, "success", response.data.message);
-        })
-        .catch((error) => {
-          console.log("error", error);
-          updateToast(toastID, "error", error?.response?.data.message);
-        });
+    const WACode = window.location.href.split("?waCode=")[1];
+    if (WACode) {
+      setWaCode(WACode);
     }
-  }, [baseURL]);
+  }, []);
+
+  console.log("waCode", waCode);
+
+  useEffect(() => {
+    if (waCode?.length > 0) {
+      if (baseURL.length === 0 && typeof window !== "undefined") {
+        loadingToast(toastID);
+        whatsappSignin(baseURL, {
+          waCode,
+          rememberMe: true,
+        })
+          .then((response) => {
+            updateToast(toastID, "success", response.data.message);
+          })
+          .catch((error) => {
+            console.log("error", error);
+            updateToast(toastID, "error", error?.data);
+          });
+      }
+    }
+  }, [baseURL, waCode]);
 
   return (
     <StoreLayoutPage title="Cloudmall Africa" padding="0px" showArrow={false}>
